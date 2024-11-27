28e85501d2320ce11e389f43b1ab823925aca789
Harriet Waninge <harrietwaninge@gmail.com>
2024-11-27 16:34:37 +0000

    fix database first 6 tasks

--- periodic_table.sql
+++ periodic_table.sql
@@ -49,8 +49,8 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.elements (
     atomic_number integer NOT NULL,
-    symbol character varying(2),
-    name character varying(40)
+    symbol character varying(2) NOT NULL,
+    name character varying(40) NOT NULL
 );
 
 
@@ -62,10 +62,9 @@ ALTER TABLE public.elements OWNER TO freecodecamp;
 
 CREATE TABLE public.properties (
     atomic_number integer NOT NULL,
-    type character varying(30),
     atomic_mass numeric(9,6) NOT NULL,
-    melting_point numeric,
-    boiling_point numeric
+    melting_point_celsius numeric NOT NULL,
+    boiling_point_celsius numeric NOT NULL
 );
 
 
@@ -90,15 +89,15 @@ INSERT INTO public.elements VALUES (1000, 'mT', 'moTanium');
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.properties VALUES (1, 'nonmetal', 1.008000, -259.1, -252.9);
-INSERT INTO public.properties VALUES (2, 'nonmetal', 4.002600, -272.2, -269);
-INSERT INTO public.properties VALUES (3, 'metal', 6.940000, 180.54, 1342);
-INSERT INTO public.properties VALUES (4, 'metal', 9.012200, 1287, 2470);
-INSERT INTO public.properties VALUES (5, 'metalloid', 10.810000, 2075, 4000);
-INSERT INTO public.properties VALUES (6, 'nonmetal', 12.011000, 3550, 4027);
-INSERT INTO public.properties VALUES (7, 'nonmetal', 14.007000, -210.1, -195.8);
-INSERT INTO public.properties VALUES (8, 'nonmetal', 15.999000, -218, -183);
-INSERT INTO public.properties VALUES (1000, 'metalloid', 1.000000, 10, 100);
+INSERT INTO public.properties VALUES (1, 1.008000, -259.1, -252.9);
+INSERT INTO public.properties VALUES (2, 4.002600, -272.2, -269);
+INSERT INTO public.properties VALUES (3, 6.940000, 180.54, 1342);
+INSERT INTO public.properties VALUES (4, 9.012200, 1287, 2470);
+INSERT INTO public.properties VALUES (5, 10.810000, 2075, 4000);
+INSERT INTO public.properties VALUES (6, 12.011000, 3550, 4027);
+INSERT INTO public.properties VALUES (7, 14.007000, -210.1, -195.8);
+INSERT INTO public.properties VALUES (8, 15.999000, -218, -183);
+INSERT INTO public.properties VALUES (1000, 1.000000, 10, 100);
 
 
 --
@@ -131,6 +130,22 @@ ALTER TABLE ONLY public.properties
 
 ALTER TABLE ONLY public.properties
     ADD CONSTRAINT properties_pkey PRIMARY KEY (atomic_number);
+
+
+--
+-- Name: elements unique_elements; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.elements
+    ADD CONSTRAINT unique_elements UNIQUE (symbol, name);
+
+
+--
+-- Name: properties properties_atomic_number_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.properties
+    ADD CONSTRAINT properties_atomic_number_fkey FOREIGN KEY (atomic_number) REFERENCES public.elements(atomic_number);
 
 
 --
