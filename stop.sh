4426ff96e6594df9f80f4a50a2d4c2a746911671
Harriet Waninge <harrietwaninge@gmail.com>
2024-12-02 10:44:52 +0000

    fix: capitalize all first letters of elements symbol comumn

--- stop.sh
+++ stop.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 pg_dump -cC --inserts -U freecodecamp periodic_table > periodic_table.sql
 
-yes | sudo apt-get install rsync
-rsync -a --exclude={'.*','periodic_table'} ./ ./periodic_table
-
-cd periodic_table
+#cd periodic_table
 #git add .
 #git commit -m "$(date)"
 #git push -u origin main
