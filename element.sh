b4449eaf5f019030dab8951e216683bcd882eb49
Harriet Waninge <harrietwaninge@gmail.com>
2024-12-04 13:45:28 +0000

    refactor: aah never mind

--- element.sh
+++ element.sh
@@ -8,14 +8,9 @@ if [[ -z $1 ]]
   echo "Please provide an element as an argument."
   else 
   #check if it is a number
-    if [[ "$1" =~ ^[0-9]+$ ]]
-     then   $1 = "'$1'" 
-     echo $1
-     else 
-           ATOMIC_NUMBER_RESULT=$($PSQL "select atomic_number, symbol, name from elements where atomic_number = $1")
-    fi  
-    
+    if [[ "$1" =~ ^[0-9]+$ ]]; then 
       # check if this element number exists 
+      ATOMIC_NUMBER_RESULT=$($PSQL "select atomic_number, symbol, name from elements where atomic_number = $1")
       #10|Ne|Neon
       if [[ -z $ATOMIC_NUMBER_RESULT ]] 
         then
@@ -27,9 +22,13 @@ if [[ -z $1 ]]
           echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a nonmetal, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
         done
       fi
-      
+      else # if is not a number
+      #check if is valid symbol or name
+       ATOMIC_VARCHAR_RESULT=$($PSQL "select * from elements where symbol = '$1' OR name = '$1")
+       echo "$ATOMIC_VARCHAR_RESULT"
 
-  
+
+    fi
   #check type of parameter? 
   # no.  input is one of atomic_number, symbol or name. 
   # 
