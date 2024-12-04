393fc862ff34ac2e16d5d78c2cbccb721efafe62
Harriet Waninge <harrietwaninge@gmail.com>
2024-12-04 13:44:08 +0000

    refactor: I'm deeper in this than I realized

--- element.sh
+++ element.sh
@@ -8,21 +8,28 @@ if [[ -z $1 ]]
   echo "Please provide an element as an argument."
   else 
   #check if it is a number
-    if [[ "$1" =~ ^[0-9]+$ ]]; then
-    # check if this element number exists 
-    ATOMIC_NUMBER_RESULT=$($PSQL "select atomic_number, symbol, name from elements where atomic_number = $1")
-     #10|Ne|Neon
+    if [[ "$1" =~ ^[0-9]+$ ]]
+     then   $1 = "'$1'" 
+     echo $1
+     else 
+           ATOMIC_NUMBER_RESULT=$($PSQL "select atomic_number, symbol, name from elements where atomic_number = $1")
+    fi  
+    
+      # check if this element number exists 
+      #10|Ne|Neon
       if [[ -z $ATOMIC_NUMBER_RESULT ]] 
-      then
+        then
         echo "This element number is not in the database."
       else 
-      INFORMATION_QUERY_RESULT=$($PSQL "select * from elements FULL JOIN properties USING(atomic_number) where atomic_number = $1")
+        INFORMATION_QUERY_RESULT=$($PSQL "select * from elements FULL JOIN properties USING(atomic_number) where atomic_number = $1")
         echo "$INFORMATION_QUERY_RESULT" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR ATOMIC_MASS BAR MELTING_POINT_CELSIUS BAR BOILING_POINT_CELSIUS BAR TYPE BAR TYPE_ID
           do
           echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a nonmetal, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
         done
       fi
-    fi
+      
+
+  
   #check type of parameter? 
   # no.  input is one of atomic_number, symbol or name. 
   # 
